630348740c78e54b578157a6c86d3a40b28b603f
Lyrimi <lyrimisin@gmail.com>
2024-09-04 13:54:54 +0200

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -1,1 +1,20 @@
-Write-Output "Hello World"
+function DoSendKeys {
+    param (
+        $SENDKEYS,
+        $WINDOWTITLE
+    )
+    $wshell = New-Object -ComObject wscript.shell;
+    IF ($WINDOWTITLE) {$wshell.AppActivate($WINDOWTITLE)}
+    Start-Sleep(1)
+    IF ($SENDKEYS) {$wshell.SendKeys($SENDKEYS)}
+}
+
+#DoSendKeys -SENDKEYS '%{f4}'
+
+while ($true) {
+    Start-Sleep(1)
+    $randnum = Get-Random(10)
+    if ($randnum -eq 0) {
+        DoSendKeys -SENDKEYS 'a'
+    }
+}
