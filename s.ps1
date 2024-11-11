45a63f21fe29ade13f3056a9d2226cabfdaea395
Lyrimi <lyrimisin@gmail.com>
2024-11-11 13:19:54 +0100

    No perhaps?

--- s.ps1
+++ s.ps1
@@ -2,5 +2,6 @@ Write-Output "hello world!"
 Write-Output "file should be gone yay :D"
 
 Set-Location $HOME\Downloads
-New-Item "No.txt" -Value "NO"
+New-Item "No.txt"
+Set-Content -Path .\No.txt -Value "No Perhaps?" 
 notepad.exe "No.txt"
